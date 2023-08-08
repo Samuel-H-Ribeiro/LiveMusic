7c54e304ff7a2b01c76f8c4823306894db3f02c9
Samuel_H_Ribeiro <samuelhribeiro07@gmail.com>
2023-08-08 16:38:10 -0300

    Criado o projeto LiveMusic

---
+++ src/main/java/br/com/sam/soundlive/SoundliveApplication.java
@@ -0,0 +1,13 @@
+package br.com.sam.soundlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundliveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sam/soundlive/SoundliveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sam.soundlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
